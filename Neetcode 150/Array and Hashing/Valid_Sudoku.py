8ad8a9fee58755800633825a37eb7f4ab9b8060f
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-01-20 21:24:59 -0500

    added solution to valid sudoku

---
+++ Neetcode 150/Array and Hashing/Valid_Sudoku.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        boxes = collections.defaultdict(set)
+        column = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                if (board[row][col] == "."):
+                    continue
+                if (board[row][col] in column[col] or board[row][col] in rows[row] or board[row][col] in boxes[
+                    (row // 3), (col // 3)]):
+                    return False
+
+                rows[row].add(board[row][col])
+                column[col].add(board[row][col])
+                boxes[(row // 3, (col // 3))].add(board[row][col])
+
+        return True
